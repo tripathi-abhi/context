01e46391317e001e07d865bb7c3c8437fce972c1
Abhishek Tripathi <abhitripathi2312@gmail.com>
2022-10-25 21:24:43 +0530

    implementation of Background and TODO functions

--- Context/main.go
+++ Context/main.go
@@ -1,2 +1,32 @@
 package context
 
+import "time"
+
+// Creating context interface
+
+type Context interface {
+	Deadline() (time.Time, bool)
+	Done() <-chan struct{}
+	Err() error
+	Value(interface{}) interface{}
+}
+
+type emptyCtx int64
+
+func Deadline() (time.Time, bool)   { return time.Time{}, false }
+func Done() <-chan struct{}         { return nil }
+func Err() error                    { return nil }
+func Value(interface{}) interface{} { return nil }
+
+var (
+	background = new(emptyCtx)
+	todo       = new(emptyCtx)
+)
+
+func Background() *emptyCtx {
+	return background
+}
+
+func TODO() *emptyCtx {
+	return todo
+}
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	Context "github.com/tripathi-abhi/context_pkg/context"
+)
+
+func main() {
+	ctx1 := Context.Background()
+	ctx2 := Context.Background()
+
+	fmt.Printf("ctx1: %v address ctx1: %p\n", *ctx1, ctx1)
+	fmt.Printf("ctx2: %v address ctx2: %p\n", *ctx2, ctx2)
 }
